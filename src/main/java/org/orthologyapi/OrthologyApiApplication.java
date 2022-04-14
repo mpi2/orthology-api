167bfd627454d7d41c024eb460dee26a166a37de
ogunes <ogunes@ebi.ac.uk>
2022-04-14 13:51:08 +0100

    Initial commit Project Created

---
+++ src/main/java/org/orthologyapi/OrthologyApiApplication.java
@@ -0,0 +1,13 @@
+package org.orthologyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrthologyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrthologyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/orthologyapi/OrthologyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orthologyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrthologyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
