1275af628d29b270b5cc9c7bc3e6f89e589e79e8
ogunes <ogunes@ebi.ac.uk>
2022-06-08 14:45:44 +0100

    reference data cluster deployment kubernetes configurations added

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -9,7 +9,7 @@ if [ -z "${DOCKER_PRODUCTION}" ]; then
     if [ -z "${EBI_PROXY}" ]; then
 
       java -Djava.security.egd=file:/dev/./urandom -jar app.jar \
-        --server.port="${port}" --spring.profiles.active=dockerreference
+        --server.port="${port}" --spring.profiles.active=docker
 
     else
 
@@ -23,55 +23,9 @@ if [ -z "${DOCKER_PRODUCTION}" ]; then
         -Dftp.proxyPort=3128 \
         -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
         -jar app.jar \
-        --server.port="${port}" --spring.profiles.active=dockerreference
+        --server.port="${port}" --spring.profiles.active=docker
 
     fi
-
-  else
-
-    if [ -z "${EBI_PROXY}" ]; then
-
-      java -Djava.security.egd=file:/dev/./urandom -jar app.jar \
-        --server.port="${port}" --spring.profiles.active=dockerreference
-
-    else
-
-      java -Djava.security.egd=file:/dev/./urandom \
-        -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
-        -Dhttps.proxyPort=3128 \
-        -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk \
-        -Dhttp.proxyPort=3128 \
-        -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-        -Dftp.proxyHost=hx-wwwcache.ebi.ac.uk \
-        -Dftp.proxyPort=3128 \
-        -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-        -jar app.jar \
-        --server.port="${port}" --spring.profiles.active=dockerreference
-
-    fi
-
-  fi
-
-else
-
-  if [ -z "${EBI_PROXY}" ]; then
-
-    java -Djava.security.egd=file:/dev/./urandom -jar app.jar \
-      --server.port="${port}" --spring.profiles.active=dockerreference
-
-  else
-
-    java -Djava.security.egd=file:/dev/./urandom \
-      -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
-      -Dhttps.proxyPort=3128 \
-      -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk \
-      -Dhttp.proxyPort=3128 \
-      -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-      -Dftp.proxyHost=hx-wwwcache.ebi.ac.uk \
-      -Dftp.proxyPort=3128 \
-      -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-      -jar app.jar \
-      --server.port="${port}" --spring.profiles.active=dockerreference
 
   fi
 
