8728004462d9660e7afff416b9a03e7e95f050ca
ogunes <ogunes@ebi.ac.uk>
2024-05-08 09:18:53 +0100

    GET All One To One All Orthologs As TSV File endpoint added

--- src/test/java/org/orthologyapi/constant/DBSetupFilesPathsConst.java
+++ src/test/java/org/orthologyapi/constant/DBSetupFilesPathsConst.java
@@ -9,6 +9,5 @@ public class DBSetupFilesPathsConst {
 
     public static final String EXPECTED_All_ORTHOLOGS_PAGEABLE_JSON =
         "expectedAllOrthologsPageable.json";
-
     public static final String INTEGRATION_TESTS_RESOURCE_PATH = "/integrationtests/";
 }
