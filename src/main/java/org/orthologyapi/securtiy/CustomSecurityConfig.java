4a2b3fe5d382f660ec8405b82fd251b789b32087
ogunes-ebi <ogunes@ebi.ac.uk>
2025-12-16 10:24:34 +0000

    spring boot version upgraded to 4.0.0

--- src/main/java/org/orthologyapi/securtiy/CustomSecurityConfig.java
+++ src/main/java/org/orthologyapi/securtiy/CustomSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,13 +16,13 @@ public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().authorizeHttpRequests()
+        http.authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.GET, "/api/ortholog/**")
             .permitAll()
-            .requestMatchers(HttpMethod.GET, "/docs.html").permitAll();
+            .requestMatchers(HttpMethod.GET, "/docs.html").permitAll());
 
 
-        http.headers().frameOptions().disable();
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return http.build();
     }
 
